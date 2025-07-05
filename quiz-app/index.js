98456590e1d603aa1e6c0ca755c28bdb8c0a2c31
Hamzaminhal <hamzaminhal2@gmail.com>
2025-07-05 10:15:29 +0500

    quizz Application code

--- quiz-app/index.js
+++ quiz-app/index.js
@@ -1,11 +1,20 @@
 let questionElement = document.getElementById("question");
 let optionsElement = document.getElementById("options");
+let userAns = null;
+let currentQuestion = 0;
+let correctAnswers = 0;
+let score = 0;
+let nextBtn = document.getElementById("nextBtn");
+let endBtn = document.getElementById("endBtn");
+let quizPart = document.getElementById("quizPart");
+let resultPart = document.getElementById("resultPart");
+let selectedDiv = document.getElementById("selected");
 
 const questions = [
   {
     question: "What is the capital of France?",
     options: ["Rome", "Paris", "Madrid", "Berlin"],
-    correctOption: "paris",
+    correctOption: "Paris",
   },
   {
     question: "How many legs does a spider have?",
@@ -25,24 +34,62 @@ const questions = [
   {
     question: "Which animal is known as the King of the Jungle?",
     options: ["Monkey", "Lion", "Girraf", "Cow"],
-    correctOption: "paris",
+    correctOption: "Lion",
   },
 ];
-let currentQuestion = 0;
 showQuestion();
 
+// Show Question
+
 function showQuestion() {
-  questionElement.innerHTML = questions[currentQuestion].question;
+  questionElement.innerHTML =
+    currentQuestion + 1 + ". " + questions[currentQuestion].question;
   optionsElement.innerHTML = "";
   questions[currentQuestion].options.map((element) => {
-    optionsElement.innerHTML += `<div><input type="radio" name="option" id="option-${element}"> ${element}</div>`;
+    optionsElement.innerHTML += `<div class="radio">
+    <input type="radio" onclick="saveAns(event)" name="option" id="option-${element}" value="${element}">
+    <label id="option-${element}" for="option-${element}">${element}</label>
+    </div>`;
   });
 }
+
+//    Next Question
+
 function nextQuestion() {
+  if (userAns == questions[currentQuestion].correctOption) {
+    correctAnswers++;
+    score += 10;
+  }
+  // debugger;
   if (currentQuestion < questions.length - 1) {
     currentQuestion++;
     showQuestion();
-  } else {
-    alert("Quiz Ended");
+    // console.log(score);
   }
+  if (currentQuestion == questions.length - 1) {
+    nextBtn.style.display = "none";
+    endBtn.style.visibility = "visible";
+  }
+}
+
+// Save Answer
+
+function saveAns(clickedAns) {
+  userAns = clickedAns.target.value;
+  // clickedAns.target.classList.add("selected");
+  console.log(selectedDiv);
+}
+
+// End Quiz
+
+function endQuiz() {
+  if (userAns == questions[currentQuestion].correctOption) {
+    correctAnswers++;
+    score += 10;
+  }
+  quizPart.style.display = "none";
+  resultPart.innerHTML = `<div class="score text-center">
+          Your Correct Ansers are : ${correctAnswers} <br />
+          Your Total score is : ${score}
+        </div>`;
 }
